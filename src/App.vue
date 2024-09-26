8137912b75c5a158ef989166449b057ddfb50583
mpsalunggg <putrasatria893@gmail.com>
2024-09-26 20:50:40 +0700

    create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <template>
-  <nav class="mb-4">
-    <ul class="flex space-x-4">
-      <li v-for="route in routes" :key="route.path">
-        <router-link :to="route.path">{{ route.label }}</router-link>
-      </li>
-    </ul>
-  </nav>
+  <NavBar />
   <main>
     <router-view />
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
-import router from "./router/router";
+import { defineComponent } from "vue";
+import NavBar from "./components/Navbar.vue";
 
 export default defineComponent({
-  setup() {
-    const routes = computed(() =>
-      router.getRoutes().map((route) => {
-        return {
-          path: route.path,
-          label: route.meta.title,
-        };
-      })
-    );
-
-    return {
-      routes,
-    };
+  components: {
+    NavBar,
   },
 });
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="w-full bg-black text-white text-center py-2">
+    <ul class="flex space-x-4 justify-center">
+      <li v-for="route in routes" :key="route.path">
+        <router-link :to="route.path">{{ route.label }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import router from "../router/router";
+
+export default defineComponent({
+  setup() {
+    const routes = computed(() =>
+      router.getRoutes().map((route) => {
+        return {
+          path: route.path,
+          label: route.meta.title,
+        };
+      })
+    );
+
+    return {
+      routes,
+    };
+  },
+});
+</script>
