eb8bb83aae0b37b0e16d299b484cb64437ff4b08
mpsalunggg <putrasatria893@gmail.com>
2024-09-27 21:15:45 +0700

    watch vue

---
+++ src/pages/UserPage.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+
+const user = ref({
+  name: 'Putra',
+  address: {
+    city: 'Palu',
+    country: 'Indonesia'
+  }
+});
+
+watch(user, (newUser, oldUser) => {
+  console.log('User object changed:');
+  console.log('New User:', newUser);
+  console.log('Old User:', oldUser);
+}, { immediate: true, deep: true });
+</script>
+
+<template>
+  <div>
+    <h3>Informasi Pengguna</h3>
+    <p>Nama: {{ user.name }}</p>
+    <p>Kota: {{ user.address.city }}</p>
+    <p>Negara: {{ user.address.country }}</p>
+
+    <input v-model="user.name" placeholder="Ubah nama pengguna" />
+    <input v-model="user.address.city" placeholder="Ubah kota pengguna" />
+    <input v-model="user.address.country" placeholder="Ubah negara pengguna" />
+  </div>
+</template>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import TodoPage from '../pages/TodoPage.vue';
+import UserPage from '../pages/UserPage.vue';
 
 const routes = [
     {
@@ -11,6 +12,11 @@ const routes = [
     {
         path: '/todo', component: TodoPage, meta: {
             title: 'ToDo',
+        },
+    },
+    {
+        path: '/user', component: UserPage, meta: {
+            title: 'User',
         },
     },
 ];
